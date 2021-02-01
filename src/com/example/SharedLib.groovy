41da2e7a6245386b4f18695c2a1a3d77920f6114
Sibendra30 <46244484+Sibendra30@users.noreply.github.com>
2021-02-01 14:35:41 +0000

    Update SharedLib.groovy

--- src/com/example/SharedLib.groovy
+++ src/com/example/SharedLib.groovy
@@ -2,8 +2,8 @@ package com.example
 
 public class SharedLib {
 
-def addNumber() {
-      println "Welcome to shared library"
+int addNumber() {
+      sh 'echo Welcome to shared library'
       def x = 5
       return x + 10;
    }
